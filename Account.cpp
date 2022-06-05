0aac292bc9f99795c7f2650d2150ca744d795b2f
Samir Rodrigues <emaildesamir@gmail.com>
2022-06-05 20:41:05 -0300

    Makefile add

--- Account.cpp
+++ Account.cpp
@@ -1,31 +1,43 @@
 
 #include <iostream>
 
-class Account{
-  private:
-    
-  public:
-    int number = 0;
-    float balance = 0.0f;
-    Account(){
-      int number = 0;
-      float balance = 0.0f;
-    }
-    Account(int acountNumber){
-      number = acountNumber;
-      balance = 5.0f;
-    }
-    int GetNumber(){
-      return number;
-    }
-    float GetBalance(){
-      std::cout << balance << std::endl;
-      return balance;
-    }
-    void Credit(int amount){      
-      balance += amount;
-    }
-    void Debit(int amount){
-      balance -= amount;
-    }
+class Account
+{
+private:
+  int number;
+  float balance;
+
+public:
+  Account()
+  {
+    number = 0;
+    balance = 0.0f;
+  }
+
+  Account(int acountNumber)
+  {
+    number = acountNumber;
+    balance = 5.0f;
+  }
+
+  int GetNumber()
+  {
+    return number;
+  }
+
+  float GetBalance()
+  {
+    std::cout << balance << std::endl;
+    return balance;
+  }
+
+  void Credit(int amount)
+  {
+    balance += amount;
+  }
+
+  void Debit(int amount)
+  {
+    balance -= amount;
+  }
 };
--- Bank.cpp
+++ Bank.cpp
@@ -2,65 +2,70 @@
 #include <list>
 #include "Account.cpp"
 
-class Bank{
-  private:
-      
-  public:
-      std::list<Account> accounts;
-      Account accountNULL;
-    Bank(){;
-    }
+class Bank
+{
+private:
+public:
+  std::list<Account> accounts;
+  Account accountNULL;
+  Bank()
+  {
+  }
 
-    void AddAccount(int accountNumber){
-      Account account(accountNumber);
-      accounts.push_back(account);
-    }
+  void AddAccount(int accountNumber)
+  {
+    Account account(accountNumber);
+    accounts.push_back(account);
+  }
 
-    float GetBalance(int accountNumber){
-      Account * account;
-      account = FindAccount(accountNumber);
-      return account->GetBalance();
-    }
+  float GetBalance(int accountNumber)
+  {
+    Account *account = new Account;
+    account = FindAccount(accountNumber);
+    return account->GetBalance();
+  }
 
-    void CreditAccount(int accountNumber, int value)
+  void CreditAccount(int accountNumber, int value)
+  {
+    Account *account;
+    account = FindAccount(accountNumber);
+    account->Credit(value);
+  }
+
+  void DebitAccount(int accountNumber, int value)
+  {
+    Account *account;
+    account = FindAccount(accountNumber);
+    account->Debit(value);
+  }
+
+  Account *FindAccount(int accountNumber)
+  {
+    Account *account_ptr;
+    for (Account account : accounts)
     {
-      Account * account;
-      account = FindAccount(accountNumber);
-      account->Credit(value);
+      if (account.GetNumber() == accountNumber)
+      {
+        account_ptr = &account;
+        return account_ptr;
+      }
     }
-    
-    void DebitAccount(int accountNumber, int value)
+    return &accountNULL;
+  }
+  bool Transfer(int accountNumberCredit, int accountNumberDebit, int amount)
+  {
+    Account *accountCredit = FindAccount(accountNumberCredit);
+    Account *accountDebit = FindAccount(accountNumberDebit);
+
+    if (accountCredit->GetNumber() == accountNULL.GetNumber() || accountDebit->GetNumber() == accountNULL.GetNumber())
     {
-      Account * account;
-      account = FindAccount(accountNumber);
-      account->Debit(value);
-    }
-    
-
-    Account *FindAccount(int accountNumber){
-      Account *account_ptr;
-      for(Account account : accounts){
-        if(account.GetNumber() == accountNumber){
-          account_ptr = &account;
-          return account_ptr;
-        }
-      }
-        return &accountNULL;
-    }
-    bool Transfer(int accountNumberCredit, int accountNumberDebit, int amount){
-      Account * accountCredit = FindAccount(accountNumberCredit);
-      Account * accountDebit = FindAccount(accountNumberDebit);
-
-      if(accountCredit->GetNumber() == accountNULL.GetNumber() || accountDebit->GetNumber() == accountNULL.GetNumber()){
-        std::cout << "Conta Não Cadastrada!" << std::endl;
-        return false;
-      }
-
-      accountCredit->Credit(amount);
-      accountDebit->Debit(amount);
-
-      return true;
-
+      std::cout << "Conta Não Cadastrada!" << std::endl;
+      return false;
     }
 
+    accountCredit->Credit(amount);
+    accountDebit->Debit(amount);
+
+    return true;
+  }
 };
