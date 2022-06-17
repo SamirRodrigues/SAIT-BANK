ffd35384dbb54a2e42fc85af4644e09d5fc62487
Samir Rodrigues <emaildesamir@gmail.com>
2022-06-16 22:15:50 -0300

    Conta Simples e Conta Bônus - limite de saldo negativo R$ -1k
    As contas do tipo Conta Simples (o tipo criado na primeira tarefa) 
    e as contas do tipo Conta Bônus (criada na segunda tarefa) devem possuir um 
    limite máximo de saldo negativo de R$ -1.000,00.

--- BonusAccount.cpp
+++ BonusAccount.cpp
@@ -1,30 +1,33 @@
 // #include <iostream>
 #include "Account.cpp"
 
-class BonusAccount : public Account{
-  protected:
-    int points;
-    // int accountType;
+class BonusAccount : Account
+{
+protected:
+  int points;
+  // int accountType;
 
-  public:
-    BonusAccount() : Account(){
-      
-    }
-    BonusAccount(int accountNumber) : Account(accountNumber){
-      points = 10;
-      // accountType = 2;
-    }
-    ~BonusAccount(){
-
-    }
-    void AddPoints(int pointsAmount) {
-      points += pointsAmount;
-    }
-    // int GetType() const{
-      // return accountType;
-    // }
-    int GetPoints() const {
-      return points;
-    }
-
+public:
+  BonusAccount() : Account()
+  {
+  }
+  BonusAccount(int accountNumber) : Account(accountNumber)
+  {
+    points = 10;
+    // accountType = 2;
+  }
+  ~BonusAccount()
+  {
+  }
+  void AddPoints(int pointsAmount)
+  {
+    points += pointsAmount;
+  }
+  // int GetType() const{
+  // return accountType;
+  // }
+  int GetPoints() const
+  {
+    return points;
+  }
 };
